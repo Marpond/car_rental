226a16f10004fde8cf9431c9ffb94c9ab185ca1d
allan <allan2fernandes@hotmail.com>
2022-05-03 20:08:15 +0200

    Changed some stored procedures

--- src/main/sql/insertNewCustomer.sql
+++
@@ -1,9 +0,0 @@
-USE DB_AutoCamperRental;
-GO
-
-CREATE PROCEDURE InsertNewCustomer @phoneNumber INT, @name VARCHAR(35), @address VARCHAR(40)
-	AS
-		BEGIN
-			INSERT INTO tbl_Customer(fld_PhoneNoNumber, fld_Name, fld_Address)
-			VALUES(@phoneNumber, @name, @address);
-		END
