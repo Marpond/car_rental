d7ea2cd243fc7294e5ba3eee8d924040ee153b90
allan <allan2fernandes@hotmail.com>
2022-05-03 20:46:11 +0200

    update

--- src/main/sql/storedprocedures.sql
+++ src/main/sql/storedprocedures.sql
@@ -110,6 +110,39 @@ CREATE PROCEDURE CreateNewPaymentEntry
             END
 GO
 
+
+CREATE PROCEDURE CreateReceiptEntry
+    @distanceDriven DOUBLE PRECISION,
+	@returnedFuel DOUBLE PRECISION,
+	@fuelPrice DOUBLE PRECISION,
+	@finalPrice DOUBLE PRECISION,
+	@mileagePrice DOUBLE PRECISION,
+	@reservationID INT,
+	@discountID INT
+		AS
+            BEGIN
+            INSERT INTO tbl_Receipt
+            (
+                fld_DistanceDriven,
+                fld_ReturnedFuel,
+                fld_FuelPrice,
+                fld_FinalPrice,
+                fld_MileagePrice,
+                fld_ReservationID,
+                fld_DiscountID
+            )
+            VALUES
+                (
+                    @distanceDriven,
+                    @returnedFuel,
+                    @fuelPrice,
+                    @finalPrice,
+                    @mileagePrice,
+                    @reservationID,
+                    @discountID
+                )
+            END
+
 create procedure SelectCamper
 as
 select * from tbl_Camper;
